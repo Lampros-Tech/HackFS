a5bde0f4826eb3053dcef01cb04cb6d78a002178
Lajja <lajja.lamprostech@gmail.com>
2022-07-13 18:25:14 +0530

    Lajja 13-07-2022

--- src/components/crypto-info/CryptoInfo.js
+++ src/components/crypto-info/CryptoInfo.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Axios from 'axios';
+import Coin from "./Coin";
 
 const CryptoInfo = () => {
+    const [listOfCoins, setListOfCoins]=useState([]);
+
+    useEffect(() =>{
+        Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+            (response)=>{
+                setListOfCoins(response.data.coins);
+                    } 
+        );
+    },[]);
     return(
         <>
-            <div>
-                Inside Crypto Info.
+            <div className='App'>
+            <div className='cryptoHeader'></div>
+            <div className='cryptoDisplay'>{
+                listOfCoins.map((coin)=>{
+                    return <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/>;
+                })
+            }</div>
             </div>
         </>
     )
