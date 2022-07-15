6b3dd0f0ef024126513ebde130ebf23a7c407977
Jaydip Patel <jaydip.lamprostech@gmail.com>
2022-07-15 18:24:08 +0530

    Jaydip 15-07-2022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /********************* DEPENDENCIES ********************/
+<<<<<<< Updated upstream
 import React, { useState, useRef, useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom' // react-router v4/v5
 import Cookies from 'universal-cookie';
@@ -15,27 +16,36 @@ import { ethers } from 'ethers';
 import metamask from "./components/mm.png";
 import coinbase from "./components/cbw.png";
 // import walletconnect from "./components/wc.png";
+=======
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"; // react-router v4/v5
+>>>>>>> Stashed changes
 
 /********************* COMPONENTS ********************/
-import Navbar from './components/Navbar';
-import Home from './components/homepage/Home';
-import CryptoInfo from './components/crypto-info/CryptoInfo';
-import AddQuestions from './components/questions/AddQuestions';
-import DisplayQuestions from './components/questions/DisplayQuestions';
-import SingleQuestion from './components/questions/SingleQuestion';
-import AddArticle from './components/questions/AddArticle';
-import Chat from './components/chat/Chat';
-import Profile from './components/users/Profile';
-import FindUsers from './components/users/FindUsers';
-import SingleUser from './components/users/SingleUser';
-import Login from './components/login/Login';
+import Navbar from "./components/Navbar";
+import Home from "./components/homepage/Home";
+import CryptoInfo from "./components/crypto-info/CryptoInfo";
+import AddQuestions from "./components/questions/AddQuestions";
+import DisplayQuestions from "./components/questions/DisplayQuestions";
+import SingleQuestion from "./components/questions/SingleQuestion";
+import AddArticle from "./components/questions/AddArticle";
+import Chat from "./components/chat/Chat";
+import Profile from "./components/users/Profile";
+import FindUsers from "./components/users/FindUsers";
+import SingleUser from "./components/users/SingleUser";
+import Login from "./components/login/Login";
 
 /********************* CSS CLASS ********************/
+<<<<<<< Updated upstream
 import './index.css';
 import './App.scss';
 
 
 
+=======
+import "./index.css";
+import "./App.css";
+>>>>>>> Stashed changes
 
 const App = () => {
 
@@ -139,15 +149,18 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/info" element={<CryptoInfo />} />
-            <Route path='/ask-question' element={<AddQuestions />} />
-            <Route path='/find-question' element={<DisplayQuestions />} />
-            <Route path='/single-question/:id' element={<SingleQuestion id={1} />} />
-            <Route path='/add-article' element={<AddArticle />} />
-            <Route path='/message/:id' element={<Chat id={1} />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/find-profile' element={<FindUsers />} />
-            <Route path='/user/:id' element={<SingleUser id={1} />} />
-            <Route path='/login' element={<Login />} />
+            <Route path="/ask-question" element={<AddQuestions />} />
+            <Route path="/find-question" element={<DisplayQuestions />} />
+            <Route
+              path="/single-question/:id"
+              element={<SingleQuestion id={1} />}
+            />
+            <Route path="/add-article" element={<AddArticle />} />
+            <Route path="/message/:id" element={<Chat id={1} />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/find-profile" element={<FindUsers />} />
+            <Route path="/user/:id" element={<SingleUser id={1} />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
         {
@@ -183,6 +196,6 @@ const App = () => {
     </>
 
   );
-}
+};
 
 export default App;
