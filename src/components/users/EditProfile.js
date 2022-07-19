83836355c692ac1220db58e88bb4b98e5b049a99
Jaydip Patel <jaydip.lamprostech@gmail.com>
2022-07-19 12:35:35 +0530

    Jaydip 19-07-2022 12:35AM

--- src/components/users/EditProfile.js
+++ src/components/users/EditProfile.js
@@ -1,24 +1,77 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import { WithContext as ReactTags } from "react-tag-input";
 
 export default function EditProfile({ closeModal }) {
   const editorRef = useRef(null);
+  const [tags, setTags] = useState([]);
+  const handleAddition = (tag) => {
+    setTags([...tags, tag]);
+    console.log(tag);
+  };
+  const handleTagClick = (index) => {
+    console.log("The tag at index " + index + " was clicked");
+  };
+  const handleDelete = (i) => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+  const KeyCodes = {
+    comma: 188,
+    enter: 13,
+  };
+  const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
   return (
     <>
       <div className="modalBackground">
         <div className="modalContainer">
-          <div className="titleCloseBtn">
-            <button onClick={() => closeModal(false)}> X </button>
-          </div>
+          {/* <div className="titleCloseBtn">
+            <img
+              onClick={() => closeModal(false)}
+              src={cancleButton}
+              alt="user avatar"
+              height="16px"
+              width="16px"
+            />
+          </div> */}
           <div className="title">
             <h1>Edit Your Profile</h1>
+            <svg
+              onClick={() => closeModal(false)}
+              version="1.1"
+              id="Capa_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 47.095 47.095"
+            >
+              <g>
+                <path
+                  d="M45.363,36.234l-13.158-13.16l12.21-12.21c2.31-2.307,2.31-6.049,0-8.358c-2.308-2.308-6.05-2.307-8.356,0l-12.212,12.21
+		              L11.038,1.906c-2.309-2.308-6.051-2.308-8.358,0c-2.307,2.309-2.307,6.049,0,8.358l12.81,12.81L1.732,36.831
+		              c-2.309,2.31-2.309,6.05,0,8.359c2.308,2.307,6.049,2.307,8.356,0l13.759-13.758l13.16,13.16c2.308,2.308,6.049,2.308,8.356,0
+                  C47.673,42.282,47.672,38.54,45.363,36.234z"
+                />
+              </g>
+            </svg>
           </div>
           <div className="body">
             <h3>Display Name</h3>
-            <input className="input-edit-profile" type="text" placeholder="Your Good Name" />
+            <input
+              className="input-edit-profile"
+              type="text"
+              placeholder="Your Good Name"
+            />
             <h3>Change Email</h3>
-            <input className="input-edit-profile" type="text" placeholder="email" />
+            <input
+              className="input-edit-profile"
+              type="text"
+              placeholder="Email"
+            />
+            <h3>Designation</h3>
+            <input
+              className="input-edit-profile"
+              type="text"
+              placeholder="e.g. 'Full Stack Developer'"
+            />
             <h3>About me</h3>
             <Editor
               onInit={(evt, editor) => (editorRef.current = editor)}
@@ -40,9 +93,22 @@ export default function EditProfile({ closeModal }) {
                   "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
               }}
             />
+            <h3>Tags</h3>
+            <div>
+              <ReactTags
+                tags={tags}
+                delimiters={delimiters}
+                handleDelete={handleDelete}
+                handleAddition={handleAddition}
+                handleTagClick={handleTagClick}
+                inputFieldPosition="inline"
+                autocomplete
+              />
+            </div>
           </div>
           <div className="footer">
-            <button className="save"
+            <button
+              className="save"
               onClick={() => {
                 closeModal(false);
               }}
@@ -50,7 +116,7 @@ export default function EditProfile({ closeModal }) {
             >
               Cancel
             </button>
-            <button className="save" >Continue</button>
+            <button className="save">Continue</button>
           </div>
         </div>
       </div>
