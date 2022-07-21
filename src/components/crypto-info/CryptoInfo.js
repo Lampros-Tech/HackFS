a3c1f13debdca930ce43a633d8b9762a182ef774
Jaydip Patel <jaydip.lamprostech@gmail.com>
2022-07-21 15:27:23 +0530

    Jaydip 21-07-22 03:27 PM

--- src/components/crypto-info/CryptoInfo.js
+++ src/components/crypto-info/CryptoInfo.js
@@ -8,38 +8,6 @@ import "./Cryptoinfo-style/Article.scss";
 import Cryptonews from "./Cryptonews";
 import "./Cryptoinfo-style/News.scss";
 
-<<<<<<< HEAD
-const CryptoInfo = ({id}) => {
-    const style = {
-        height: 600,
-      };
-    const [listOfCoins, setListOfCoins]=useState([]);
-    useEffect(() =>{
-        Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
-            (response)=>{
-                setListOfCoins(response.data.coins);            } 
-            );
-         },[]);
-    
-    return(
-        <>
-            
-            <div className="crypto-header">
-            {
-               listOfCoins.map((coin)=>{
-                return <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/>;
-                })
-                }
-            </div>
-            <div className="crypto-main-container">
-            <CryptoArticle/>
-            <Cryptonews/>
-            </div>
-           
-        </>
-    )
-}
-=======
 const CryptoInfo = () => {
   const [listOfCoins, setListOfCoins] = useState([]);
   useEffect(() => {
@@ -49,7 +17,6 @@ const CryptoInfo = () => {
       }
     );
   }, []);
->>>>>>> f9f275a0ea19c7552a09b74690978a048671599b
 
   return (
     <>
