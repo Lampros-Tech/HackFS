16edff743bdf447a8c5a4c499682f66aa9b3e966
Lajja <lajja.lamprostech@gmail.com>
2022-07-23 10:32:34 +0530

    Lajja 23-07-2022 10:30 AM

--- src/components/crypto-info/Cryptonews.js
+++ src/components/crypto-info/Cryptonews.js
@@ -14,11 +14,10 @@ const Cryptonews = () => {
         "X-RapidAPI-Host": "crypto-news-live3.p.rapidapi.com",
       },
     };
-
     axios
       .request(options)
       .then((response) => {
-        // console.log(response.data);
+        console.log(response.data);
         setApiData(response.data);
       })
       .catch((error) => {
@@ -27,22 +26,18 @@ const Cryptonews = () => {
   }, []);
 
   console.log(apiData);
-
-  const first7Apidata = apiData?.slice(0, 9);
+  const first7Apidata = apiData?.slice(0, 11);
 
   return (
     <>
       <div className="crypto-news">
         <div className="rightcolumn">
           <div className="news">
-            <h2>Populer News</h2>
+            <h2>News</h2>
             {first7Apidata?.map((data, _index) => (
               <div key={_index}>
                 <a href={data.url}>
-                  <div className="apidata">
-                    <p>{data.title}</p>
-                  </div>
-                  {/* <p>{data.source}</p> */}
+                  <p>{data.title}</p>
                 </a>
               </div>
             ))}
